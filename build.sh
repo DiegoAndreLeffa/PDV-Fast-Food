91b1cb442d181e05ebb2932b247a35d787b08df0
DiegoAndre Leffa <diegodocanto@gmail.com>
2024-01-22 09:55:08 -0300

    feat: deploy

--- build.sh
+++ build.sh
@@ -2,12 +2,20 @@
 # exit on error
 set -o errexit
 
+# Instalação de dependências
 yarn
 
+# Compilação do projeto
 yarn build
 
+# Execução das migrações
 yarn typeorm migration:run -d dist/data-source
 
+# Copia arquivos SQL para a pasta dist
 ./node_modules/.bin/copyfiles -u 1 src/*.sql dist/
 
-node ./dist/scriptSql
+# Muda para o diretório dist
+cd dist
+
+# Executa o script SQL
+node scriptSql
